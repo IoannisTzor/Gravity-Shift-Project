77257dfdea3ebf183ab19c10ebafd994edab295b
Tzortzatos Ioannis <johntzortzatos2005@gmail.com>
2026-06-18 12:26:35 +0300

    added a basic pause menu

--- Assets/Scripts/UiManager.cs
+++ Assets/Scripts/UiManager.cs
@@ -8,30 +8,70 @@ public class UiManager : MonoBehaviour
     public TextMeshProUGUI highScoreText;
     public static UiManager Instance;
     public GameObject gameOverPanel;
+    public GameObject pausePanel;
+    public bool isPaused;
     
+
+
+
     void Awake()
     {
         Instance = this;                  
     }
     void Start()
     {
-     gameOverPanel.SetActive(false);   
+     gameOverPanel.SetActive(false);
+     pausePanel.SetActive(false);   
+    }
+    
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        pausePanel.SetActive(true);
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        //add a timer
+        pausePanel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void QuitGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MenuScene");
     }
     
     // Update is called once per frame
     void Update()
     {
         scoreText.text = "Score: "+ Mathf.FloorToInt(GameManager.Instance.ScoreGet());
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+        
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else if (GameManager.Instance.IsGameOver() == false)
+            {
+                PauseGame();
+            }
+        }
     }
     public void showGameOver()
     {
 
         gameOverPanel.SetActive(true);
+        Time.timeScale = 1f;
         highScoreText.text = "High Score: "+ Mathf.FloorToInt(PlayerPrefs.GetFloat("HighScore", 0f));
         finalScoreText.text = "Final Score: "+ Mathf.FloorToInt(GameManager.Instance.ScoreGet());
     }
     public void RestartGame()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
